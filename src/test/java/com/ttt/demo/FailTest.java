4b365083e15ec32551d66d1f0f9b7290ad85500c
Trinh Ngoc Tuyen <tuyentn@3tit.vn>
2026-03-26 10:07:08 +0700

    test CI/CD trigger

--- src/test/java/com/ttt/demo/FailTest.java
+++ src/test/java/com/ttt/demo/FailTest.java
@@ -9,7 +9,7 @@ public class FailTest {
   @Test
   void testSanityCheck() {
     int expected = 10;
-    int actual = 5;
+    int actual = 10;
     assertEquals(expected, actual, "Giá trị không khớp!");
   }
 }
